4031e16253c3076b1d9a07ca30e8b147aa911cba
pc-magas <katanophoros@gmail.com>
2019-05-22 23:02:15 +0300

    Testing Exceptions

--- tests/Rover/RoverTest.php
+++ tests/Rover/RoverTest.php
@@ -5,7 +5,10 @@ namespace Tests\Rover;
 use PHPUnit\Framework\TestCase;
 use Rover\Constants;
 use Rover\Rover;
+
 use Rover\Exceptions\InvalidCommandException;
+use Rover\Exceptions\TooManyCommandsException;
+
 
 class RoverTest extends TestCase
 {
@@ -15,5 +18,25 @@ class RoverTest extends TestCase
         $rover=new Rover(1,2,Constants::ORIENTATION_NORTH);
         $this->expectException(InvalidCommandException::class);
         $rover->processCommand($command);
+    }
+
+    public function testInvalidMultiCommand()
+    {
+        $command="HRMHV";
+        $rover=new Rover(1,2,Constants::ORIENTATION_NORTH);
+        $this->expectException(TooManyCommandsException::class);
+        $rover->processCommand($command);
+    }
+
+    public function testValidMultiCommand()
+    {
+        $commands=[Constants::COMMAND_ROT_LEFT,
+            Constants::COMMAND_MOVE,
+            Constants::COMMAND_MOVE,
+            Constants::COMMAND_ROT_RIGHT
+        ];
+        $rover=new Rover(1,2,Constants::ORIENTATION_NORTH);
+        $this->expectException(TooManyCommandsException::class);
+        $rover->processCommand(implode('',$commands));
     }
 }
