88a7640d0d9c992efa86753e716568bcc595e12d
pc-magas <katanophoros@gmail.com>
2019-05-22 22:00:14 +0300

    Run the tests

--- tests/Rover/UtilsTest.php
+++ tests/Rover/UtilsTest.php
@@ -7,12 +7,26 @@ use Rover\Utils;
 
 class UtilsTest extends TestCase
 {
-    public function testRegexCorrentSingle()
+    public function testRegexCorrectSingle()
     {
         $commands=[Constants::COMMAND_ROT_LEFT,Constants::COMMAND_ROT_RIGHT,Constants::COMMAND_MOVE];
         foreach($commands as $command){
             $result=Utils::verifyCommand($command);
             $this->assertTrue($result);
         }
+    }
+
+    public function testRegexIncorrect()
+    {
+        $invalidCommand="H";
+        $result=Utils::verifyCommand($invalidCommand);
+        $this->assertFalse($result);
+    }
+
+    public function testRegexMultipleCommands()
+    {
+        $commands=Constants::COMMAND_ROT_LEFT.Constants::COMMAND_ROT_LEFT.Constants::COMMAND_ROT_LEFT.Constants::COMMAND_MOVE.Constants::COMMAND_ROT_RIGHT;
+        $result=Utils::verifyCommand($commands);
+        $this->assertTrue($result);
     }
 }
